1dcd967063c7ddd5df590631526b48d042d8f8e4
SFDCsid <118118743+SFDCsid@users.noreply.github.com>
2022-11-28 22:06:25 +0530

    Add files via upload

--- xrbia/force-app/main/default/lwc/toggle/toggle.js
+++ xrbia/force-app/main/default/lwc/toggle/toggle.js
@@ -1,29 +1,99 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import userAPEX from '@salesforce/apex/TOOGLEAPEX.userAPEX';
+import USERACTIVE_FIELD from '@salesforce/schema/User.lwcActive__c';
+
+//import USER_OBJECT from "@salesforce/schema/User";
+//import ID_FIELD from "@salesforce/schema/User.Id";
+//import CHECKBOX_FIELD from "@salesforce/schema/User.lwcActive__c";
+//import { updateRecord } from "lightning/uiRecordApi";
+
 export default class Toggle extends LightningElement {
+    recordId = UserInfo.getUserId();
+    selectedFields = [USERACTIVE_FIELD];
 
 
-    handleChange(event){
-       console.log(event.target.checked);
-       if(event.target.checked == true){
-        UserInfo.getUserId = true;
-       }
+
+/*
+    checkUncheck;
+
+
+
     
-    }
-
-@wire(userAPEX)
-toggle({ error, data}){
-    var usertoggle=[];
-    if(data){
-        alert('tog');
-        this.usertoggle = data;
-        console.log('usertoggle'+JSON.stringify(this.usertoggle.lwcActive__c));
-    }
+       @api recordId;
+        checKBox;
     
-    else if (error) {
-        this.data = undefined;
-    }
-   
-}
+        handleChange(event) {
+            // console.log('t'+event.target.checked);
+            if (event.target.checked === true) {
+                alert('t');
+                this.checKBox = 'true';
+                console.log('tf' + this.checKBox);
+                const fields = {};
+                fields[ID_FIELD.fieldApiName] = this.recordId;
+                fields[CHECKBOX_FIELD.fieldApiName] = this.checKBox;
+    
+                const recordInput = {
+                    fields: fields
+                };
+                updateRecord(recordInput).then((record) => {
+                    console.log(record);
+                });
+            }
+            if (event.target.checked === false) {
+                alert('f');
+                this.checKBox = 'false';
+                console.log('tf' + this.checKBox);
+                const fields = {};
+                fields[ID_FIELD.fieldApiName] = this.recordId;
+                fields[CHECKBOX_FIELD.fieldApiName] = this.checKBox;
+    
+                const recordInput = {
+                    fields: fields
+                };
+                updateRecord(recordInput).then((record) => {
+                    console.log(record);
+                });
+            }
+    
+        }*/
+
+
+    /* ====================== TO GET CHECKBOX VALUE FRO SALESFORCE ======================= */
+    /*
+    userData;
+    @wire(userAPEX)
+    getuser({ error, data }) {
+
+
+        if (data) {
+           
+            this.userData = data.lwcActive__c;
+            console.log('userdata' + JSON.stringify(this.userData));
+
+            if (this.userData == true) {
+                console.log('true');
+                this.checkUncheck = true;
+             
+            }
+            if (this.userData == false) {
+                console.log('false');
+                this.checkUncheck = false;
+               
+            }
+            
+         
+
+        }
+        else if (error) {
+            this.data = undefined;
+        }
+
+
+    }*/
+
+
+
+
+
 
 }
