fca15a0bb3546a2240628b1155f15284abf42a02
SFDCsid <118118743+SFDCsid@users.noreply.github.com>
2022-12-03 22:13:50 +0530

    Add files via upload

--- xrbia/force-app/main/default/lwc/testComp/testComp.js
+++ xrbia/force-app/main/default/lwc/testComp/testComp.js
@@ -1,3 +1,4 @@
+
 import { LightningElement, wire, track, api } from 'lwc';
 import projectMethod from '@salesforce/apex/PRACTICECLASS.projectMethod';
 import sectorMethod from '@salesforce/apex/PRACTICECLASS.sectorMethod';
@@ -18,16 +19,17 @@ import unitHtml2 from '@salesforce/apex/PRACTICECLASS.unitHtml2';
 ///////////////////////
 import ACCOUNT_OBJECT from '@salesforce/schema/QUOTATION__c';
 import AGGREMENT_VALUE from '@salesforce/schema/QUOTATION__c.Aggrement_Value__c';
-//import GRAND_TOTAL from '@salesforce/schema/QUOTATION__c.Grand_Total__c';
-//import GST_AMOUNT from '@salesforce/schema/QUOTATION__c.GST_AMO__c';
+import GRAND_TOTAL from '@salesforce/schema/QUOTATION__c.GrandTotal__c';
+import GST_AMOUNT from '@salesforce/schema/QUOTATION__c.Gst__c';
 import GST_PERCENTAGE from '@salesforce/schema/QUOTATION__c.Gst_Percentage__c';
-//import OTHER_CHARGES from '@salesforce/schema/QUOTATION__c.Other_Charges1__c';
+import OTHER_CHARGES from '@salesforce/schema/QUOTATION__c.Other__c';
 import OTHERCHARGES_GST from '@salesforce/schema/QUOTATION__c.Other_Charges_Gst__c';
 import REGISTERATION_CHARGES from '@salesforce/schema/QUOTATION__c.Registeration_Charges__c';
-//import STAMPDUTY_AMOUNT from '@salesforce/schema/QUOTATION__c.Stamp_Duty_Amount__c';
+import STAMPDUTY_AMOUNT from '@salesforce/schema/QUOTATION__c.StampDuty__c';
 import STAMPDUTY_PERCENTAGE from '@salesforce/schema/QUOTATION__c.Stamp_Duty_Percentage__c';
 import UNIT_QUT from '@salesforce/schema/QUOTATION__c.Unit__c';
 import NAME_QUT from '@salesforce/schema/QUOTATION__c.Name';
+
 
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -37,7 +39,32 @@ import { NavigationMixin } from "lightning/navigation";
 
 import loop from '@salesforce/apex/PRACTICECLASS.loop';
 
-export default class PractiseLwc extends NavigationMixin(LightningElement) {
+export default class TestComp extends NavigationMixin(LightningElement) {
+
+    gstamountOutput;
+
+    gstpercentval;
+    agrementval;
+
+    
+
+    gstpercentOnchange(event) {
+        this.gstpercentval = event.target.value;
+        this.gstamountOutputmethod(); //call other funtion 
+    }
+
+
+    agrementOnchange(event) {
+        this.agrementval = event.target.value;
+        this.gstamountOutputmethod();
+    }
+
+    gstamountOutputmethod(){
+       this.gstamountOutput =  this.agrementval * this.gstpercentval/100;
+    
+    }
+
+   
 
 
     //////////////////////////////////////////////
@@ -46,7 +73,7 @@ export default class PractiseLwc extends NavigationMixin(LightningElement) {
     //areDetailsVisible=false;
 
 
-    
+
     handleClick(event) {
         var rdValue = event.target.value;
         this.recordId = rdValue;
@@ -73,13 +100,13 @@ export default class PractiseLwc extends NavigationMixin(LightningElement) {
 
     accountObject = ACCOUNT_OBJECT;
     aggrementvalueField = AGGREMENT_VALUE;
-    //grandtotalField = GRAND_TOTAL;
-    //gstamountField = GST_AMOUNT;
+    grandtotalField = GRAND_TOTAL;
+    gstamountField = GST_AMOUNT;
     gstpercentageField = GST_PERCENTAGE;
-    //otherchargesField = OTHER_CHARGES;
+    otherchargesField = OTHER_CHARGES;
     otherchargesgstField = OTHERCHARGES_GST;
     registerationchargesField = REGISTERATION_CHARGES;
-    //stampdutyamountField = STAMPDUTY_AMOUNT;
+    stampdutyamountField = STAMPDUTY_AMOUNT;
     stampdutypercentageField = STAMPDUTY_PERCENTAGE;
     unitqutField = UNIT_QUT;
     namequtField = NAME_QUT;
@@ -90,7 +117,7 @@ export default class PractiseLwc extends NavigationMixin(LightningElement) {
     message = 'Sample Message';
     variant = 'error';
 
-    handleAccountCreated(){
+    handleAccountCreated() {
         const toast = new ShowToastEvent({
             title: 'Quotation Created',
             message: 'success',
@@ -509,9 +536,9 @@ export default class PractiseLwc extends NavigationMixin(LightningElement) {
 
     }
 
-   
 
-   
+
+
 
 
 
