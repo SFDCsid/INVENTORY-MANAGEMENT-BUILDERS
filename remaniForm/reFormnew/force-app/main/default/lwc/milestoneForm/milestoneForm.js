e86dac74195567d5d118d3c591e5994bfbd9d160
SFDCsid <118118743+SFDCsid@users.noreply.github.com>
2022-12-26 23:55:07 +0530

    Add files via upload

--- remaniForm/reFormnew/force-app/main/default/lwc/milestoneForm/milestoneForm.js
+++ remaniForm/reFormnew/force-app/main/default/lwc/milestoneForm/milestoneForm.js
@@ -1,4 +1,4 @@
-import { LightningElement, track, wire, api} from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
 import recRecords from '@salesforce/apex/ReqClass.recRecords';
 import { getRecord } from 'lightning/uiRecordApi';
 import BillMethod from '@salesforce/apex/ReqClass.BillMethod';
@@ -6,87 +6,101 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { NavigationMixin } from 'lightning/navigation';
 
 const columns = [
-    { label: 'Requisition Name', fieldName: 'recordLink', type: 'url',
-        typeAttributes: {label: { fieldName: 'Name' },  
-        target: '_blank'},
-        sortable: true },
+    {
+        label: 'Requisition Name', fieldName: 'recordLink', type: 'url',
+        typeAttributes: {
+            label: { fieldName: 'Name' },
+            target: '_blank'
+        },
+        sortable: true
+    },
     {
         label: 'Milestone Name', fieldName: 'Milestone__c', type: 'url',
-        typeAttributes: {label: { fieldName: 'milestone_name__c' },  
-        target: '_blank'},
-        sortable: true  
-    },  
-    { label: 'Bill Of Material', fieldName: 'MaterialLink', type: 'url',
-        typeAttributes: {label: { fieldName: 'bill__c' },  
-        target: '_blank'},
-        sortable: true 
+        typeAttributes: {
+            label: { fieldName: 'milestone_name__c' },
+            target: '_blank'
+        },
+        sortable: true
     },
-    { label: 'Material', fieldName: 'MaterialLink', type: 'url',
-        typeAttributes: {label: { fieldName: 'Material_Name__c' },  
-        target: '_blank'},
-        sortable: true 
+    {
+        label: 'Bill Of Material', fieldName: 'MaterialLink', type: 'url',
+        typeAttributes: {
+            label: { fieldName: 'bill__c' },
+            target: '_blank'
+        },
+        sortable: true
     },
-    { label: 'Requisition Quantity', fieldName: 'Requisition_Quantity__c'},
-    { label: 'Store', fieldName: 'Store__c'},
-    { label: 'Issued Quantity', fieldName: 'Issued_Quantity__c'}
-  
+    {
+        label: 'Material', fieldName: 'MaterialLink', type: 'url',
+        typeAttributes: {
+            label: { fieldName: 'Material_Name__c' },
+            target: '_blank'
+        },
+        sortable: true
+    },
+    { label: 'Requisition Quantity', fieldName: 'Requisition_Quantity__c' },
+    { label: 'Store', fieldName: 'Store__c' },
+    { label: 'Issued Quantity', fieldName: 'Issued_Quantity__c' }
+
 ];
 
 export default class MilestoneForm extends NavigationMixin(LightningElement) {
-    
+
     @track bomId;
-    @track wireId;
+
     @track billdata;
     @track selectedMaster;
     @track contactDataWrp;
     columns = columns;
-    @track itemList = [{ id: 0, Milestone__c: null}];
+    @track itemList = [{ id: 0, Milestone__c: null }];
     @track blankRow = [];
     @track index = 0;
     keyIndex = 0;
-    @wire(getRecord, { recordId: '$selectedMaster', fields: [ MASTER_NAME_FIELD] })
-    savedRecordIdWire({error,data}) {
-        if(data){
+    @wire(getRecord, { recordId: '$selectedMaster', fields: [MASTER_NAME_FIELD] })
+    savedRecordIdWire({ error, data }) {
+        if (data) {
             this.masterName = data.fields.Name.value;
-           
-        }else if(error){
+
+        } else if (error) {
             window.alert(JSON.stringify(error));
         }
     }
 
-    handleMasterId(event){
-        this.wireId = event.detail.value[0];
+    handleMasterId(event) {
+
         let MasterId = event.detail.value[0];
-        console.log('master id: '+ MasterId);
-        
-        if(MasterId !== undefined){
+        console.log('master id: ' + MasterId);
+
+        if (MasterId !== undefined) {
             this.selectedMaster = MasterId;
             this.contactDataWrp = [];
-            recRecords({mpId : this.selectedMaster}).then(data => {
-                var templeadList = [];  
-                for (var i = 0; i < data.length; i++) {  
+            recRecords({ mpId: this.selectedMaster }).then(data => {
+                var templeadList = [];
+                for (var i = 0; i < data.length; i++) {
                     let tempRecord = Object.assign({}, data[i]); //cloning object  
                     console.log(tempRecord);
-                    tempRecord.recordLink = "/" + tempRecord.Id;  
-                    tempRecord.MaterialLink = "/"+ tempRecord.Material__c;
-                    tempRecord.milestoneLink = "/"+ tempRecord.Milestone__c;
-                    templeadList.push(tempRecord);  
-                }  
+                    tempRecord.recordLink = "/" + tempRecord.Id;
+                    tempRecord.MaterialLink = "/" + tempRecord.Material__c;
+                    tempRecord.milestoneLink = "/" + tempRecord.Milestone__c;
+                    templeadList.push(tempRecord);
+                }
                 this.contactDataWrp = templeadList;
-               
+                console.log('contactDataWrp: ' + JSON.stringify(this.contactDataWrp));
+
+
             }).catch(error => {
                 console.log(error);
             })
         }
     }
-    
+
     addRow() {
         ++this.keyIndex;
-        var newItem = [{ id: this.keyIndex, Milestone__c: this.selectedMaster}];//
+        var newItem = [{ id: this.keyIndex, Milestone__c: this.selectedMaster }];//
         this.itemList = this.itemList.concat(newItem);
-        console.log('item list size: '+ itemList.length);
+        console.log('item list size: ' + itemList.length);
     }
-    
+
     removeRow(event) {
         if (this.itemList.length >= 2) {
             this.itemList = this.itemList.filter(function (element) {
@@ -96,25 +110,25 @@ export default class MilestoneForm extends NavigationMixin(LightningElement) {
     }
 
     handleSubmit() {
-       
-        
+
+
         /*var isVal = true;
         this.template.querySelectorAll('lightning-input-field').forEach(element => {
             isVal = isVal && element.reportValidity();
         });
         if (isVal) {*/
-            this.template.querySelectorAll('lightning-record-edit-form').forEach(element => {
-                if(element.id != 'first-46' && element.id == ''){
-                    console.log('element id: '+element.id);
-                    element.submit();
-                   
-                 }
-            });
+        this.template.querySelectorAll('lightning-record-edit-form').forEach(element => {
+            if (element.id != 'first-46' && element.id == '') {
+                console.log('element id: ' + element.id);
+                element.submit();
 
-           
-           
-         //   this.navigateToViewMilestonePage();
-            
+            }
+        });
+
+
+
+        //   this.navigateToViewMilestonePage();
+
         //}
     }
     navigateToViewMilestonePage() {
@@ -143,24 +157,24 @@ export default class MilestoneForm extends NavigationMixin(LightningElement) {
            this.contactDataWrp = [];
         }
     }*/
-    handleSuccess(){
-       this.navigateToViewMilestonePage(); 
-        }
+    handleSuccess() {
+        this.navigateToViewMilestonePage();
+    }
 
-        @wire(BillMethod, { Id: '$selectedMaster' })
-        billDetails({ data, error }) {
-            if (data) {
-                this.billdata = data;
-                console.log('billdata'+JSON.stringify(this.billdata));
-                this.error = undefined;
-               
-               
+    @wire(BillMethod, { Id: '$selectedMaster' })
+    billDetails({ data, error }) {
+        if (data) {
+            this.billdata = data;
+            console.log('billdata' + JSON.stringify(this.billdata));
+            this.error = undefined;
 
-              
-            }
-            else if (error) {
-                this.error = error;
-                this.billdata = undefined;
-            }
+
+
+
         }
+        else if (error) {
+            this.error = error;
+            this.billdata = undefined;
+        }
+    }
 }
--- remaniForm/reFormnew/force-app/main/default/lwc/storeForm/storeForm.js
+++ remaniForm/reFormnew/force-app/main/default/lwc/storeForm/storeForm.js
@@ -1,3 +1,43 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
+import recRecords from '@salesforce/apex/storeFormClass.recRecords';
+const columns = [
+    { label: 'Material', fieldName: 'Material_Name__c'},
+    { label: 'Requisition Quantity', fieldName: 'Requisition_Quantity__c'}];
+export default class StoreForm extends LightningElement {
 
-export default class StoreForm extends LightningElement {}
+    @track contactDataWrp;
+    columns = columns;
+    @track selectedMaster;
+    
+
+    handleMasterId(event) {
+
+        let MasterId = event.detail.value[0];
+        console.log('master id: ' + MasterId);
+
+        if (MasterId !== undefined) {
+            this.selectedMaster = MasterId;
+            this.contactDataWrp = [];
+            recRecords({ mpId: this.selectedMaster }).then(data => {
+                var templeadList = [];
+                for (var i = 0; i < data.length; i++) {
+                    let tempRecord = Object.assign({}, data[i]); //cloning object  
+                    console.log(tempRecord);
+                    tempRecord.recordLink = "/" + tempRecord.Id;
+                    tempRecord.MaterialLink = "/" + tempRecord.Material__c;
+                    tempRecord.milestoneLink = "/" + tempRecord.Milestone__c;
+                    templeadList.push(tempRecord);
+                }
+                this.contactDataWrp = templeadList;
+                console.log('contactDataWrp: ' + JSON.stringify(this.contactDataWrp));
+
+
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
+
+}
+
+
